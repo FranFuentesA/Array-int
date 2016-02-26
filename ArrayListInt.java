c69fbb3969dd9db184eec4c4f81a54acf833f32f
FranFuentesA <fran_fuentes20@hotmail.com>
2016-02-26 11:14:12 +0100

    Commit primero

---
+++ ArrayListInt.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+/**
+ * Write a description of class ArrayListInt here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArrayListInt
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class ArrayListInt
+     */
+    public ArrayListInt()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void sampleMethod()
+    {
+        
+    }
+}
